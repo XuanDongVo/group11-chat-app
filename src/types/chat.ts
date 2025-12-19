43c643fb76d7b117225014ca56545c86d20a16ee
Trí Đức <149232709+letriduc116@users.noreply.github.com>
2025-12-19 15:42:38 +0700

    create folder structure

---
+++ src/types/chat.ts
@@ -0,0 +1,20 @@
+// Chat Header
+export interface ChatHeaderProps {
+  avatar: string;
+  name: string;
+  status?: string;
+}
+
+// Message
+export interface MessageBubbleProps {
+  text: string;
+  mine?: boolean;
+}
+
+// Sidebar
+export interface SidebarItemProps {
+  avatar: string;
+  name: string;
+  lastMessage: string;
+  active?: boolean;
+}
