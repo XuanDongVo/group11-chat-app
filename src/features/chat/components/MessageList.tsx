a8e05f31d2c2ae038b4778759a34d94f7c2466a3
Trí Đức <149232709+letriduc116@users.noreply.github.com>
2025-12-27 11:36:18 +0700

    fix lỗi hiển thị message list

--- src/features/chat/components/MessageList.tsx
+++ src/features/chat/components/MessageList.tsx
@@ -6,13 +6,16 @@ export default function MessageList({
 }: {
   messages: ChatMessage[];
 }) {
+  const currentUser = localStorage.getItem("username"); 
+//  const myUsername = localStorage.getItem("username"); 
   return (
     <div className="chat-messages">
       {messages.map((msg, idx) => (
         <MessageBubble
           key={idx}
           text={msg.content}
-          mine={msg.from === "me"} 
+          mine={msg.from !== currentUser}
+          // mine={msg.from === myUsername}
         />
       ))}
     </div>
